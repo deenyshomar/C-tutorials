02d37dba17ea95eb9953fee43839e6a929869a11
deenyshomar <deenyshomar@gmail.com>
2022-10-15 02:56:11 +0100

    while loop operation

---
+++ conditional_statements/while_loop.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * while loop operation
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	int a = 10; /* local variable defination */
+
+	while (a < 20) /* while loop execution */
+	{
+		printf("value of a : %d\n", a);
+		a++;
+	}
+
+	return (0);
+}
