ff0f25bdcf4242562f84d9cb9ec63ba0f9cfa9e4
deenyshomar <deenyshomar@gmail.com>
2022-11-24 06:28:15 +0100

    Summing of two different matrix

--- arrays/sumof2Ds.c
+++ arrays/sumof2Ds.c
@@ -5,23 +5,44 @@ int main()
 	int a[2][3], b[2][3], c[2][3];
 	int i, j;
 
-	printf("Enter the first matrix: \n");
+	printf("Enter the first matrix: \n\n");
 	for (i = 0; i < 2; i++)
 	{
 		for (j = 0; j < 3; j++)
 		{
-			scanf("%d\n", &a[i][j]);
+			scanf("%d", &a[i][j]);
 		}
 	}
-	printf("Enter the second matrix: \n");
+	printf("Enter the second matrix: \n\n");
 	for (i = 0; i < 2; i++)
 	{
 		for (j = 0; j < 3; j++)
 		{
-			scanf("%d\n", &b[2][3]);
+			scanf("%d", &b[i][j]);
 		}
 	}
-	printf("The sum of the 2 matrix is :\n");
+	putchar('\n');
+	printf("The first matrix is : \n\n");
+	for (i = 0; i < 2; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			printf("%d\t", a[i][j]);
+		}
+		putchar('\n');
+	}
+	putchar('\n');
+	printf("The second matrix is : \n\n");
+	for (i = 0; i < 2; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			printf("%d\t", b[i][j]);
+		}
+		putchar('\n');
+	}
+	putchar('\n');
+	printf("The sum of the 2 matrix is :\n\n");
 	for (i = 0; i < 2; i++)
 	{
 		for (j = 0; j < 3; j++)
@@ -31,5 +52,6 @@ int main()
 		}
 		putchar('\n');
 	}
+	putchar('\n');
 	return (0);
 }
