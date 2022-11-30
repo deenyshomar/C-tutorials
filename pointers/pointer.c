3bfe7ca0ccfdb04774d938bccca85993c9f66061
deenyshomar <deenyshomar@gmail.com>
2022-11-30 04:50:56 +0100

    decrement and increment on pointers

--- pointers/pointer.c
+++ pointers/pointer.c
@@ -9,10 +9,21 @@ int main(void)
 {
 	int n;
 	int *p;
+	int **q;
+	int ***r;
 
 	n = 98;
 	p = &n;
+	q = &p;
+	r = &q;
+	printf("Value of 'n': %d\n", n);
 	printf("Address of 'n': %p\n", &n);
 	printf("Value of 'p': %p\n", p);
+	printf("Address of 'p' : %p\n", &p);
+	printf("Value of 'q': %p\n", q);
+	printf("Address of q : %p\n", &q);
+	printf("Value of 'r': %p\n", r);
+	printf("Address of 'r': %p\n", &r);
+	
 	return (0);
 }
